78c4a07cda0d2dc9d1f14d6de9781ca63829a5c3
chandu-sanga <vtu27286@veltech.edu.in>
2026-04-01 10:05:15 +0530

    Time: 4 ms (73.32%), Space: 49.4 MB (53.93%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res=new ArrayList<>();
+        if(root==null)return res;
+        dfs(root,"",res);
+        return res;
+    }
+    
+    void dfs(TreeNode node,String path,List<String> res){
+        if(node==null)return;
+        
+        if(path.length()==0) path+=node.val;
+        else path+="->"+node.val;
+        
+        if(node.left==null && node.right==null){
+            res.add(path);
+            return;
+        }
+        
+        dfs(node.left,path,res);
+        dfs(node.right,path,res);
+    }
+}
