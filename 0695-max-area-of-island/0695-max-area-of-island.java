5cafa932580899c9e86901e2e31d98fd1888d71e
chandu-sanga <vtu27286@veltech.edu.in>
2026-05-07 17:59:24 +0530

    Time: 101 ms (3.21%), Space: 46.3 MB (81.95%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -0,0 +1,27 @@
+class Solution {
+        public int maxAreaOfIsland(int[][] grid) {
+                int max = 0;
+
+                        for (int i = 0; i < grid.length; i++) {
+                                    for (int j = 0; j < grid[0].length; j++) {
+                                                    if (grid[i][j] == 1) {
+                                                                        max = Math.max(max, dfs(grid, i, j));
+                                                                                        }
+                                                                                                    }
+                                                                                                            }
+
+                                                                                                                    return max;
+                                                                                                                        }
+
+                                                                                                                            int dfs(int[][] g, int i, int j) {
+                                                                                                                                    if (i < 0 || j < 0 || i >= g.length || j >= g[0].length || g[i][j] == 0)
+                                                                                                                                                return 0;
+
+                                                                                                                                                        g[i][j] = 0;
+
+                                                                                                                                                                return 1 + dfs(g, i + 1, j)
+                                                                                                                                                                                 + dfs(g, i - 1, j)
+                                                                                                                                                                                                  + dfs(g, i, j + 1)
+                                                                                                                                                                                                                   + dfs(g, i, j - 1);
+                                                                                                                                                                                                                       }
+                                                                                                                                                                                                                       }
