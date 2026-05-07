95b423d5a022a512cf97f66d4aba1b6464fa7d64
chandu-sanga <vtu27286@veltech.edu.in>
2026-05-07 18:02:52 +0530

    Time: 5 ms (84.14%), Space: 46.9 MB (45.07%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int[] findOrder(int n, int[][] p) {
+            List<List<Integer>> g = new ArrayList<>();
+                    int[] in = new int[n];
+
+                            for (int i = 0; i < n; i++)
+                                        g.add(new ArrayList<>());
+
+                                                for (int[] e : p) {
+                                                            g.get(e[1]).add(e[0]);
+                                                                        in[e[0]]++;
+                                                                                }
+
+                                                                                        Queue<Integer> q = new LinkedList<>();
+
+                                                                                                for (int i = 0; i < n; i++) {
+                                                                                                            if (in[i] == 0)
+                                                                                                                            q.offer(i);
+                                                                                                                                    }
+
+                                                                                                                                            int[] ans = new int[n];
+                                                                                                                                                    int idx = 0;
+
+                                                                                                                                                            while (!q.isEmpty()) {
+                                                                                                                                                                        int x = q.poll();
+                                                                                                                                                                                    ans[idx++] = x;
+
+                                                                                                                                                                                                for (int y : g.get(x)) {
+                                                                                                                                                                                                                if (--in[y] == 0)
+                                                                                                                                                                                                                                    q.offer(y);
+                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                        }
+
+                                                                                                                                                                                                                                                                return idx == n ? ans : new int[0];
+                                                                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                                                                    }
